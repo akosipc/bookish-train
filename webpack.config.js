598f00c75f795cea36b182f5a8a6d315eab48d6d
Adrian Peterson Co <adrianpetersonco@gmail.com>
2019-05-03 23:16:11 +0800

    Adds webpack compiler for scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,30 @@
 const path = require('path')
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
-module.exports = {
+const devMode = process.env.NODE_ENV === 'development'
+
+const config = {
   mode: process.env.NODE_ENV,
   context: path.join(__dirname, 'src'),
-  entry: [
-    './main.js'
-  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'www'),
+    compress: true,
+    port: 9000
+  },
+  stats: {
+    warnings: false
+  }
+}
+
+const jsConfig = Object.assign({}, config, {
+  entry: ['./main.js'],
   output: {
     path: path.join(__dirname, 'www'),
     filename: 'bundle.js'
   },
-  plugins: [
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
-  ],
   module: {
     rules: [
       {
@@ -29,12 +36,40 @@ module.exports = {
       }
     ]
   },
-  devServer: {
-    contentBase: path.join(__dirname, 'www'),
-    compress: true,
-    port: 9000
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+})
+
+const cssConfig = Object.assign({}, config, {
+  entry: ['./stylesheets/main.scss'],
+  output: {
+    path: path.join(__dirname, 'www'),
+    filename: 'bundle.css'
   },
-  stats: {
-    warnings: false
-  }
-}
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { hmr: devMode }
+          },
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+})
+
+module.exports = [jsConfig, cssConfig]
